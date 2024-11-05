4db60c1cc4535f1b4dea9d25aa9b77dd350d6efb
Adithya S Ubaradka <adithyau9101@gmail.com>
2024-11-05 22:06:42 +0530

    navbar improvement

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -1,77 +1,91 @@
-'use client'
+"use client";
 import React from "react";
-import { FaBars } from "react-icons/fa6";
+import { MdLogin } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
+import { GrGallery } from "react-icons/gr";
+import { FaRegComment } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { FaFilePen } from "react-icons/fa6";
+
 import "./Navbar.css";
 import Link from "next/link";
 
 function Navbar(props) {
-  const [isClicked, setClicked] = React.useState(false);
-
-  function toggleHamburger() {
-    setClicked(!isClicked);
-  }
-
   return (
-    <header>
-      <div className="navbar">
-        <nav>
-          <Link className="logo" href="/">
-            Easel
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="logo">
+          <Link href="/">
+            <div className="link-item">
+              <div className="link-text">Easel</div>
+            </div>
           </Link>
-
-          <ul className={isClicked ? "nav-links active" : "nav-links"}>
-            <li>
-              <Link className={props.tab === "home" ? "active" : null} href="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={props.tab === "posts" ? "active" : null}
-                href="/Posts"
-              >
-                Posts
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={props.tab === "gallery" ? "active" : null}
-                href="/Gallery"
-              >
-                Gallery
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={props.tab === "signup" ? "active" : null}
-                href="/Signup"
-              >
-                Sign Up
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={props.tab === "login" ? "active" : null}
-                href="/Login"
-              >
-                Log In
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={props.tab === "admin" ? "active" : null}
-                href="/Admin"
-              >
-                Admin
-              </Link>
-            </li>
-          </ul>
-          <div className="icon" onClick={toggleHamburger}>
-            <FaBars />
-          </div>
-        </nav>
-      </div>
-    </header>
+        </li>
+        <li className="nav-item">
+          <Link className={props.tab === "home" ? "active" : null} href="/">
+            <div className="link-item">
+              <FaHome className="icon" />
+              <div className="link-text">Home</div>
+            </div>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={props.tab === "posts" ? "active" : null}
+            href="/Posts"
+          >
+            <div className="link-item">
+              <FaRegComment className="icon" />
+              <div className="link-text">Posts</div>
+            </div>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={props.tab === "gallery" ? "active" : null}
+            href="/Gallery"
+          >
+            <div className="link-item">
+              <GrGallery className="icon" />
+              <div className="link-text">Gallery</div>
+            </div>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={props.tab === "login" ? "active" : null}
+            href="/Login"
+          >
+            <div className="link-item">
+              <MdLogin className="icon" />
+              <div className="link-text">Log In</div>
+            </div>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={props.tab === "signup" ? "active" : null}
+            href="/Signup"
+          >
+            <div className="link-item">
+              <FaFilePen className="icon" />
+              <div className="link-text">Sign Up</div>
+            </div>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={props.tab === "admin" ? "active" : null}
+            href="/Admin"
+          >
+            <div className="link-item">
+              <FaUserCircle className="icon" />
+              <div className="link-text">Admin</div>
+            </div>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
