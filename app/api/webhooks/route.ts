43d8ace15f7a21bfbc797edf683213be48f6ba37
Adithya S Ubaradka <adithyau9101@gmail.com>
2024-12-10 23:56:50 +0530

    handle user deletion and changed webhook to typescript

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -4,7 +4,7 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 
-export async function POST(request) {
+export async function POST(request : Request) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
 
   if (!SIGNING_SECRET) {
@@ -25,14 +25,14 @@ export async function POST(request) {
   const payload = await request.json();
   const body = JSON.stringify(payload);
 
-  let event;
+  let event: WebhookEvent;
 
   try {
     event = webhook.verify(body, {
       "svix-id": svix_id,
       "svix-signature": svix_signature,
       "svix-timestamp": svix_timestamp,
-    });
+    }) as WebhookEvent;
   } catch (error) {
     console.error("Could not verify webhook", error);
     return new Response("Verification Error!", { status: 400 });
@@ -59,6 +59,8 @@ export async function POST(request) {
       return new Response("Missing data", { status: 400 });
     } else {
       try {
+        await sql`DELETE FROM Comments WHERE user_id = ${id};`;
+        await sql`DELETE FROM Posts WHERE user_id = ${id};`;
         await sql`DELETE FROM Users WHERE user_id = ${id};`;
       } catch (error) {
         return NextResponse.json({ error: error.message }, { status: 500 });
