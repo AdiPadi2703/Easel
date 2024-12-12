b70a0e26f03e8a266e603cb4337ea986250b57ee
Adithya Ubaradka <122197400+AdiPadi2703@users.noreply.github.com>
2024-12-12 22:49:53 +0530

    Update and rename page.tsx to page.jsx

--- app/Gallery/search/page.jsx
+++ app/Gallery/search/page.jsx
@@ -1,12 +1,6 @@
 import GalleryPage from "../page";
 
-interface PageProps {
-  searchParams: {
-    [key: string]: string | string[];
-  };
-}
-
-async function Page({ searchParams }: PageProps) {
+async function Page({ searchParams }) {
   const { query } = await searchParams;
 
   return (
@@ -17,3 +11,4 @@ async function Page({ searchParams }: PageProps) {
 }
 
 export default Page;
+
